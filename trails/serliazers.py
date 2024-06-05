a9db55e517c99806f6bcafce1bbd4c9102a27a2b
andre059 <andreyperm159@mail.ru>
2024-06-05 16:50:12 +0500

    creating a user and trails

--- trails/serliazers.py
+++ trails/serliazers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from trails.models import Trail
+from trails.models import Trail, Rule, Notification
 
 
-class TrailsSerializer(serializers.ModelSerializer):
+class TrailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trail
+        fields = '__all__'
+
+
+class RuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rule
+        fields = '__all__'
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
         fields = '__all__'
