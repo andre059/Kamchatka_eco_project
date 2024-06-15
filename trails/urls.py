9548ea0cf45a80d32f0e6ed6bd2ca57a40d5d738
andre059 <andreyperm159@mail.ru>
2024-06-15 22:18:12 +0500

    improvements in urls

--- trails/urls.py
+++ trails/urls.py
@@ -6,7 +6,7 @@ from trails.views import ClusterInfoNalichevoView, ParkListView, InformationNaly
 app_name = TrailsConfig.name
 
 urlpatterns = [
-    path('cluster_info_nalichevo', ClusterInfoNalichevoView.as_view(), name='cluster_info_nalichevo'),
-    path('parks', ParkListView.as_view(), name='parks'),
-    path('information_nalychevo', InformationNalychevoViews.as_view(), name='information_nalychevo'),
+    path('cluster_info_nalichevo/', ClusterInfoNalichevoView.as_view(), name='cluster_info_nalichevo'),
+    path('parks/', ParkListView.as_view(), name='parks'),
+    path('information_nalychevo/', InformationNalychevoViews.as_view(), name='information_nalychevo'),
 ]
